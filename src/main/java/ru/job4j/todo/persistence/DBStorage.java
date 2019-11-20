9e7ea4ea8346479eb9481cd38289b4b83f222440
Aleksandr Karpachov <xalexandrx9@gmail.com>
2019-11-20 13:18:35 +0200

    Lambdas and the wrapper pattern. [# 182627]

--- src/main/java/ru/job4j/todo/persistence/DBStorage.java
+++ src/main/java/ru/job4j/todo/persistence/DBStorage.java
@@ -2,9 +2,11 @@ package ru.job4j.todo.persistence;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import ru.job4j.todo.models.Item;
@@ -19,43 +21,42 @@ public class DBStorage implements IStore {
 
 	@Override
 	public void addItem(Item item) {
-		Session session = factory.openSession();
-		session.beginTransaction();
-		session.save(item);
-		session.getTransaction().commit();
-		session.close();
-		
+		this.tx(session -> session.save(item));
 	}
 
 	@Override
 	public void updateItem(Item item) {
-		Session session = factory.openSession();
-		session.beginTransaction();
-		session.update(item);
-		session.getTransaction().commit();
-		session.close();
+		this.tx(session -> {
+			session.update(item);
+			return true;
+			}
+		);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Item> getAll() {
-		List<Item> result = new ArrayList<>();
-		Session session = factory.openSession();
-		session.beginTransaction();
-		result = session.createQuery("from Item").list();
-		session.getTransaction().commit();
-		session.close();
-		return result;
+		return this.tx(session -> session.createQuery("from Item").list());
 	}
 
 	@Override
 	public Item getByID(Long id) {
-		Session session = factory.openSession();
-		session.beginTransaction();
-		Item result = session.get(Item.class, id);
-		session.getTransaction().commit();
-		session.close();
-		return result;
+		return this.tx(session -> session.get(Item.class, id));
+	}
+	
+	private <T> T tx(final Function<Session, T> command) {
+	    final Session session = factory.openSession();
+	    final Transaction tx = session.beginTransaction();
+	    try {
+	        T rsl = command.apply(session);
+	        tx.commit();
+	        return rsl;
+	    } catch (final Exception e) {
+	        session.getTransaction().rollback();
+	        throw e;
+	    } finally {
+	        session.close();
+	    }
 	}
 	
 }
